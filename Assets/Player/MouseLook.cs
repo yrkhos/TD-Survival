86ce25bbaf04e60a0fbe6ba603132df862d4d003
yrkhos <138087960+yrkhos@users.noreply.github.com>
2024-09-13 06:13:46 +0200

    Mise a jour du joueur

--- Assets/Player/MouseLook.cs
+++ Assets/Player/MouseLook.cs
@@ -9,8 +9,7 @@ public class MouseLook : MonoBehaviour
 
     public Transform playerBody;
 
-    float xRotation = -90f;
-    float yRotation = 90f;
+    float xRotation = 0f;
 
     void Start()
     {
@@ -22,12 +21,8 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-
-        yRotation -= mouseX;
-        yRotation = Mathf.Clamp(-90f, yRotation, 90f);
-
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, 90f, 90f);
+        xRotation = Mathf.Clamp (0f, xRotation, 0f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -6,8 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public static PlayerController Instance;
 
-    [SerializeField]
-    private Animator anim;
+    /*[SerializeField]
+    private Animator anim;*/
 
     [SerializeField]
     PlayerInput input;
@@ -28,7 +28,7 @@ public class PlayerController : MonoBehaviour
         GameObject.DontDestroyOnLoad(this.gameObject);
 
         Cursor.lockState = CursorLockMode.Locked;
-        anim = gameObject.GetComponent<Animator>();
+        //anim = gameObject.GetComponent<Animator>();
     }
 
     void Update()
@@ -37,49 +37,21 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(input.InputFront))
         {
             transform.Translate(0, 0, devant * Time.deltaTime);
-            anim.SetBool("Marche", true);
-            anim.SetBool("Arriere", false);
-            anim.SetBool("Idle", false);
-            anim.SetBool("CoteGauche", false);
-            anim.SetBool("CoteDroit", false);
-        }
-        else
-        {
-            anim.SetBool("Idle", true);
-            anim.SetBool("Arriere", false);
-            anim.SetBool("Marche", false);
-            anim.SetBool("CoteGauche", false);
-            anim.SetBool("CoteDroit", false);
         }
 
         if (Input.GetKey(input.InputBack))
         {
             transform.Translate(0, 0, -arriere * Time.deltaTime);
-            anim.SetBool("Arriere", true);
-            anim.SetBool("Marche", false);
-            anim.SetBool("Idle", false);
-            anim.SetBool("CoteGauche", false);
-            anim.SetBool("CoteDroit", false);
         }
 
         if (Input.GetKey(input.InputLeft))
         {
             transform.Translate(-cote * Time.deltaTime, 0, 0);
-            anim.SetBool("CoteGauche", true);
-            anim.SetBool("Arriere", false);
-            anim.SetBool("CoteDroit", false);
-            anim.SetBool("Idle", false);
-            anim.SetBool("Marche", false);
         }
 
         if (Input.GetKey(input.InputRight))
         {
             transform.Translate(cote * Time.deltaTime, 0, 0);
-            anim.SetBool("CoteDroit", true);
-            anim.SetBool("Arriere", false);
-            anim.SetBool("CoteGauche", false);
-            anim.SetBool("Idle", false);
-            anim.SetBool("Marche", false);
         }
     }
 }
